c5c81f61fbec934231149255ef3d397335ff1b4d
Sushmadavasam <sushmada878@gmail.com>
2023-06-23 19:37:35 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sushma(request):
+    return render(request,'sushma.html')
+def Dhruv(request):
+    return render(request,'Dhruv.html')
